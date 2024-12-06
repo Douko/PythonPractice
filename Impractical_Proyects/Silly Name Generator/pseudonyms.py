6ca70d8cd688bab8e4ca716de320fa6805fee4dc
Douko <bryanpq3237@gmail.com>
2024-12-06 00:34:20 -0500

    change

--- Impractical_Proyects/Silly Name Generator/pseudonyms.py
+++ Impractical_Proyects/Silly Name Generator/pseudonyms.py
@@ -31,3 +31,17 @@ last  = ('Appleyard', 'Bigmeat', 'Bloominshine','Boogerbottom',
          'Turnipseed', 'Vinaigrette', 'Walkinstick', 'Wallbanger','Weewax',
          'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
          'Woolysocks')
+
+while True:
+    firstName = random.choice(first)
+    lastName  = random.choice(last)
+
+    print("\n\n")
+    print("{} {}".format(firstName, lastName), file=sys.stderr)
+    print("\n\n")
+
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+    if try_again.lower() == "n":
+        break
+
+input("\nPress Enter to exit.")
